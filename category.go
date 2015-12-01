9191661e1651f9090964d264f1af5ec696e8bdeb
Sam Mulube <sam@umbrellium.co.uk>
2015-12-01 13:18:29 +0000

    Promote Category to a full struct

--- category.go
+++ category.go
@@ -1,30 +1,60 @@
 package thingfulx
 
 // Category is a simple type alias for a string
-type Category string
+type Category struct {
+	// Name is the name of the category
+	Name string
 
-const (
+	// UID is a unique URL safe ID
+	UID string
+}
+
+var (
 	// Home is the exported const representing the Thingful Home category
-	Home = Category("home")
+	Home = Category{
+		Name: "Home",
+		UID:  "home",
+	}
 
 	// Environment is the exported const representing the Thingful Environment category
-	Environment = Category("environment")
+	Environment = Category{
+		Name: "Environment",
+		UID:  "environment",
+	}
 
 	// Transport is the exported const representing the Thingful Transport category
-	Transport = Category("transport")
+	Transport = Category{
+		Name: "Transport",
+		UID:  "transport",
+	}
 
 	// Health is the exported const representing the Thingful Health category
-	Health = Category("health")
+	Health = Category{
+		Name: "Health",
+		UID:  "health",
+	}
 
 	// Energy is the exported const representing the Thingful Energy category
-	Energy = Category("energy")
+	Energy = Category{
+		Name: "Energy",
+		UID:  "energy",
+	}
 
 	// Flora is the exported const representing the Thingful Flora category
-	Flora = Category("flora & fauna")
+	Flora = Category{
+		Name: "Flora & Fauna",
+		UID:  "flora-and-fauna",
+	}
 
 	// Experiment is the exported const representing the Thingful Experiment category
-	Experiment = Category("experiment")
+	Experiment = Category{
+		Name: "Experiment",
+		UID:  "experiment",
+	}
 
 	// Miscellaneous is the exported const representing the Thingful Miscellaneous category
-	Miscellaneous = Category("miscellaneous")
+	Miscellaneous = Category{
+		Name: "Miscellaneous",
+		UID:  "miscellaneous",
+	}
 )
