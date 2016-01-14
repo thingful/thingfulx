9373338f0a46b9953f0d110f7eef6908d6b3e259
Sam Mulube <sam@umbrellium.co.uk>
2016-01-14 00:42:10 +0000

    Add new error type for missing config, and add constructors

--- errors.go
+++ errors.go
@@ -18,22 +18,42 @@ func (e *ErrUnexpectedResponse) Error() string {
 	return fmt.Sprintf("thingfulx: unexpected HTTP response code, got %s", e.Status)
 }
 
+// NewErrUnexpectedResponse is a constructor for creating ErrUnexpectedResponse
+// instances
+func NewErrUnexpectedResponse(status string) *ErrUnexpectedResponse {
+	return &ErrUnexpectedResponse{status}
+}
+
+// ErrMissingConfig is a simple struct for reporting a missing config variable.
+// It contains a single Variable attribute which should contain the name of the
+// missing variable.
+type ErrMissingConfig struct {
+	Variable string
+}
+
+// Error is the implementation of the default error interface. Returns a simple
+// string reporting the received status
+func (e *ErrMissingConfig) Error() string {
+	return fmt.Sprintf("thingfulx: missing config variable - '%s'", e.Variable)
+}
+
+// NewErrMissingConfig is a constructor for creating ErrMissingConfig instances
+func NewErrMissingConfig(variable string) *ErrMissingConfig {
+	return &ErrMissingConfig{variable}
+}
+
 var (
 	// ErrInvalidData is a generic error that can be returned if retrieved data
 	// is invalid in any way
 	ErrInvalidData = errors.New("thingfulx: invalid data")
 
-	// ErrInvalidLocationData is an error indicating that there is a problem with
-	// the location of a fetched thing
+	// ErrInvalidLocation is an error indicating that there is a problem with the
+	// location of a fetched thing
 	ErrInvalidLocation = errors.New("thingfulx: missing or invalid location data")
 
-	// ErrInvalidTimeData is an error indicating that there is a problem with
-	// parsing the time value from a retrieved thing
+	// ErrInvalidTime is an error indicating that there is a problem with parsing
+	// the time value from a retrieved thing
 	ErrInvalidTime = errors.New("thingfulx: invalid time data")
-
-	// ErrMissingCredentials is an error that should be returned if a fetcher is
-	// missing required credentials
-	ErrMissingCredentials = errors.New("thingfulx: missing required credentials")
 
 	// ErrInvalidURL is an error used to flag an invalid url
 	ErrInvalidURL = errors.New("thingfulx: invalid url")
--- errors_test.go
+++ errors_test.go
@@ -5,11 +5,19 @@ import (
 )
 
 func TestErrUnexpectedResponse(t *testing.T) {
-	e := ErrUnexpectedResponse{
-		Status: "404",
-	}
+	e := NewErrUnexpectedResponse("404")
 
 	msg := "thingfulx: unexpected HTTP response code, got 404"
+
+	if e.Error() != msg {
+		t.Errorf("Unexpected response, expected '%s', got '%s'", msg, e.Error())
+	}
+}
+
+func TestErrMissingConfig(t *testing.T) {
+	e := NewErrMissingConfig("API_KEY")
+
+	msg := "thingfulx: missing config variable - 'API_KEY'"
 
 	if e.Error() != msg {
 		t.Errorf("Unexpected response, expected '%s', got '%s'", msg, e.Error())
