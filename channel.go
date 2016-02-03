336e34497490ba999bd3bdbd967a7c28736facef
Sam Mulube <sam@umbrellium.co.uk>
2016-02-03 23:37:27 +0000

    Add json metadata, and make unit a simple string.

--- channel.go
+++ channel.go
@@ -7,8 +7,8 @@ import (
 // Channel represents an individual data channel being published by a Thing out
 // there in the world somewhere.
 type Channel struct {
-	ID         string
-	Value      string
-	RecordedAt time.Time
-	Unit       Unit
+	ID         string    `json:"id"`
+	Value      string    `json:"value"`
+	RecordedAt time.Time `json:"recorded_at"`
+	Unit       string    `json:"units"`
 }
--- thing.go
+++ thing.go
@@ -8,50 +8,50 @@ import (
 // indexed on some upstream data provider.
 type Thing struct {
 	// Title is a short descriptive lable for the thing
-	Title string
+	Title string `json:"title"`
 
 	// Description is a long form description about the thing
-	Description string
+	Description string `json:"description"`
 
 	// Metadata is a comma separated tags - additional metadata about the Thing,
 	// that isn't necessarily encoded into the feed.
-	Metadata string
+	Metadata string `json:"-"`
 
 	// Category is a thingfulx.Category instance - choose from the declared categories
-	Category *Category
+	Category *Category `json:"-"`
 
 	// Webpage is a URL to a human intelligible page about the thing. This
 	// doesn't have to be a unique page for the thing as typically one might not
 	// be available. In that case, this could be a link to a page describing the
 	// type of thing, or just a link to the main homepage of the project.
-	Webpage string
+	Webpage string `json:"webpage"`
 
 	// DataURL is a string containing the actual URL we go to fetch data from for
 	// this thing. This string must be unique for every thing indexed. We can
 	// achieve this for things that don't make a real unique URL available by
 	// adding a synthetic identifier to the URL string, i.e.
 	// http://example.com?city=Indiana#section_id=1234
-	DataURL string
+	DataURL string `json:"datasource"`
 
 	// IndexedAt is a Time object indicating the timestamp when the thing was
 	// indexed by Thingful
-	IndexedAt time.Time
+	IndexedAt time.Time `json:"indexed_at"`
 
 	// Lng is the longitude of the Thing, represented as a decimal value
-	Lng float64
+	Lng float64 `json:"longitude"`
 
 	// Lat is the latitude of the Thing, represented as a decimal value
-	Lat float64
+	Lat float64 `json:"latitude"`
 
 	// Provider is a data structure containing information about the upstream
 	// data provider.
-	Provider *Provider
+	Provider *Provider `json:"-"`
 
 	// Visibility indicates what level of access the upstream data provider
 	// offers, Closed, Shared or Open.
-	Visibility Visibility
+	Visibility Visibility `json:"visibility"`
 
 	// Channels are a slice of Channel objects which capture the actual data of
 	// the thing
-	Channels []Channel
+	Channels []Channel `json:"channels"`
 }
