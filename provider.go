6c53ad88983e578e489bf97210f04d63edea630b
Sam Mulube <sam@umbrellium.co.uk>
2016-04-04 17:47:48 +0100

    Remove confusing dataurl attribute from providers.

--- provider.go
+++ provider.go
@@ -15,11 +15,6 @@ type Provider struct {
 	// Webpage should contain a link to the home page of the upstream provider.
 	Webpage *url.URL
 
-	// DataURL should contain a URL representing the base URL data is actually
-	// retrieved from. This might be different from the human intended Webpage
-	// described previously
-	DataURL *url.URL
-
 	// Description should contain some text describing the data provider.
 	Description string
 }
