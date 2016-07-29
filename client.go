4add759b44e7ed2aad6a17fab81d30f78ffe23e1
Popcore <smallzed@gmail.com>
2016-07-29 10:30:46 +0100

    remove context related functions and variables from the Client

--- client.go
+++ client.go
@@ -5,12 +5,6 @@ import (
 	"time"
 )
 
-const (
-	// ClientToken is the key we use when setting a token on the
-	// cleint clientInfo property
-	ClientToken = "client-token"
-)
-
 // Client specifies an interface we will use for making outgoing requests
 // within each Fetcher. Currently everything is HTTP, but that won't
 // necessarily remain the case, so we add a layer of abstraction meaning we can
@@ -20,11 +14,6 @@ type Client interface {
 	// simply ensures we set the correct user agent header before making the
 	// request
 	DoHTTP(req *http.Request) (*http.Response, error)
-
-	// GetClientInfo is a utility method that allows the client to pass
-	// extra information about its context to the fetcher that will
-	// consume it.
-	GetClientInfo() map[string]interface{}
 }
 
 // NewClient is a constructor function that instantiates and returns a new
@@ -39,31 +28,10 @@ func NewClient(userAgent string, timeout time.Duration) Client {
 	}
 }
 
-// NewClientWithInfo is a constructor function that instantiates and returns a
-// new client struct, setting the clientInfo property
-func NewClientWithInfo(userAgent string, timeout time.Duration, clientInfo map[string]interface{}) Client {
-	return &client{
-		userAgent: userAgent,
-		http: &http.Client{
-			Timeout: timeout,
-		},
-		clientInfo: clientInfo,
-	}
-}
-
-func (c *client) GetClientInfo() map[string]interface{} {
-	if len(c.clientInfo) != 0 {
-		return c.clientInfo
-	}
-
-	return nil
-}
-
 // client is our concrete implementation of the Client interface
 type client struct {
-	userAgent  string
-	http       *http.Client
-	clientInfo map[string]interface{}
+	userAgent string
+	http      *http.Client
 }
 
 // DoHTTP is a thin wrapper around the `Do` method on `net/http.Client`. It
