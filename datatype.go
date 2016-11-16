2bb5df24ef544294cf35beb81522198c712932d1
Sam Mulube <sam@umbrellium.co.uk>
2016-11-16 21:21:55 +0000

    Add a couple of convenience methods to ensure we return valid type info

--- datatype.go
+++ datatype.go
@@ -1,5 +1,9 @@
 package thingfulx
 
+import (
+	"strconv"
+)
+
 // DataType is a type alias for our iota constant below representing the type
 // of the channel's value.
 type DataType int
@@ -26,4 +30,24 @@ var dataTypes = []string{
 // String returns a string representation of the const.
 func (d DataType) String() string {
 	return dataTypes[d-1]
+}
+
+// VerifyNumberType confirms that a given string value is really a number as we
+// will later claim.
+func VerifyNumberType(val string) (string, error) {
+	_, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return "", err
+	}
+	return val, nil
+}
+
+// VerifyBooleanType confirms that a given string is really a boolean type as
+// we will later claim.
+func VerifyBooleanType(val string) (string, error) {
+	_, err := strconv.ParseBool(val)
+	if err != nil {
+		return "", err
+	}
+	return val, nil
 }
--- datatype_test.go
+++ datatype_test.go
@@ -14,3 +14,33 @@ func TestDataType(t *testing.T) {
 	assert.Equal(t, "url", URLType.String())
 	assert.Equal(t, "binary", BinaryType.String())
 }
+
+func TestVerifyNumberType(t *testing.T) {
+	validNumbers := []string{"0", "12.2", "-83.12"}
+	for _, number := range validNumbers {
+		got, err := VerifyNumberType(number)
+		assert.Nil(t, err)
+		assert.Equal(t, number, got)
+	}
+
+	invalidNumbers := []string{"", " ", "foo", "12.2.4"}
+	for _, number := range invalidNumbers {
+		_, err := VerifyNumberType(number)
+		assert.Error(t, err)
+	}
+}
+
+func TestVerifyBooleanType(t *testing.T) {
+	validBooleans := []string{"0", "1", "True", "TRUE", "false", "F", "f"}
+	for _, val := range validBooleans {
+		got, err := VerifyBooleanType(val)
+		assert.Nil(t, err)
+		assert.Equal(t, val, got)
+	}
+
+	invalidBooleans := []string{"", " ", "foo", "0.0"}
+	for _, val := range invalidBooleans {
+		_, err := VerifyBooleanType(val)
+		assert.Error(t, err)
+	}
+}
