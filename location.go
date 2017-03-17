4088148c9771e9d9b9f2a8baabf08a47547dc55f
Popcore <smallzed@gmail.com>
2017-03-17 13:53:50 +0000

    add Elevation propr

--- location.go
+++ location.go
@@ -3,6 +3,7 @@ package thingfulx
 // Location is a data structure which holds an individual Thing's
 // geographic coordinates
 type Location struct {
-	Lng float64 `json:"longitude"`
-	Lat float64 `json:"latitude"`
+	Lng       float64 `json:"longitude"`
+	Lat       float64 `json:"latitude"`
+	Elevation float64 `json:"elevation,omitempty"`
 }
