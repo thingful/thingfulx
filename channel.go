93158eadc4422c2dd8beab0e3d256983ca8e91bd
Popcore <smallzed@gmail.com>
2017-03-17 12:52:48 +0000

    don't forget to comment exported types

--- channel.go
+++ channel.go
@@ -11,16 +11,22 @@ type Channel struct {
 	Data         Data              `json:"data"`
 }
 
+// ChannelMetadata is an object which contains any metadata required to
+// semantically descibe a measurement collected by a Thing.
 type ChannelMetadata struct {
 	Prop string `json:"property"`
 	Val  string `json:"value"`
 }
 
+// Observation contains information about where and when the Observation
+// was recorded.
 type Observation struct {
 	RecordedAt time.Time `json:"recorded_at"`
 	Location   Location  `json:"location"`
 }
 
+// Data holds the actual value returned by the Thing as well as the
+// concrete type of the value itself.
 type Data struct {
 	Type string      `json:"type"`
 	Val  interface{} `json:"value"`
