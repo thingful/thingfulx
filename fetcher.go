605df4ec8ce4fcd19b1c8c71584dba1eb117c7c0
Popcore <smallzed@gmail.com>
2017-03-17 12:20:31 +0000

    rename interfaces for clarity

--- fetcher.go
+++ fetcher.go
@@ -1,31 +1,30 @@
 package thingfulx
 
 import (
-	"time"
-
 	"context"
+	"time"
 )
 
-// FetcherBuilder is type definition for the constructor functions that new
+// IndexerBuilder is type definition for the constructor functions that new
 // Fetchers must implement.
-type FetcherBuilder func() (Fetcher, error)
+type IndexerBuilder func() (Indexer, error)
 
-// Fetcher is the main interface for things that know how to fetch some
+// Indexer is the main interface for things that know how to fetch some
 // resource from somewhere handling any protocol weirdness or authentication
 // requirements and return back a slice of parsed Thing instances. The
 // interface is agnostic in terms of protocol so an implementing class is free
 // to perform whatever steps required to get the data.
-type Fetcher interface {
-	Seeder
+type Indexer interface {
+	URLProvider
 
-	Crawler
+	Fetcher
 
 	Parser
 }
 
-// Seeder in the interface used by Pomelo to generate a minimum set of URLS that
+// URLProvider in the interface used by Pomelo to generate a minimum set of URLS that
 // should be indexed for a host.
-type Seeder interface {
+type URLProvider interface {
 	// URLS returns the smallest set of URLs required to completely index the upstream
 	// data provider. The returned slice might be a single URL for hosts that publish
 	// relatively few things, or it might be a whole bunch for hosts that have pages
@@ -37,18 +36,18 @@ type Seeder interface {
 	URLS(ctx context.Context, client Client, delay time.Duration) ([]string, error)
 }
 
-// Crawler is the interface that must be implemented for performing
+// Fetcher is the interface that must be implemented for performing
 // the task of going and getting data from some remote data provider, and
 // returning a slice of Thing objects extracted from that data source.
 // Certain providers might require multiple requests from different URLs to
 // actually retrieve the data so within this interface they are free to do
 // this.
-type Crawler interface {
-	// Crawl takes as parameters an instance of the Context Interface for
+type Fetcher interface {
+	// Fetch takes as parameters an instance of the Context Interface for
 	// request-scoped values, the url we want to get data from, a Client object
 	// that the crawler will use to actually make the request.
 	// It returns the data provider response as a slice of bytes.
-	Crawl(ctx context.Context, url string, client Client) ([]byte, error)
+	Fetch(ctx context.Context, url string, client Client) ([]byte, error)
 }
 
 // Parser is the interface used to provide a full representation of a Thing.
