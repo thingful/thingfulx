d4f72197c9663b4e23d1d0d327ae89670ee2fc52
dotmancando <dotmancando@gmail.com>
2017-06-06 16:35:08 +0100

    just marshal json to see

--- dataLicense_test.go
+++ dataLicense_test.go
@@ -1,14 +1,18 @@
 package thingfulx
 
 import (
-	// "fmt"
-	"github.com/davecgh/go-spew/spew"
+	"fmt"
+	// "github.com/davecgh/go-spew/spew"
+	"encoding/json"
 	"testing"
 )
 
 func TestLicense(t *testing.T) {
-	spew.Dump(CC_0_V1)
-	spew.Dump(CC_BY_NC_V3)
-	spew.Dump(CC_BY_SA_V4)
-	spew.Dump(OGL_V3)
+	j, _ := json.Marshal(CC_BY_NC_V3)
+	fmt.Println(string(j))
+
+	// spew.Dump(CC_0_V1)
+	// spew.Dump(CC_BY_NC_V3)
+	// spew.Dump(CC_BY_SA_V4)
+	// spew.Dump(OGL_V3)
 }
