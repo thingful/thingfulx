42f5c7d368d5968f67c2f4f7d16bda86166d65c3
dotmancando <dotmancando@gmail.com>
2017-06-07 11:14:15 +0100

    add test cases for DataLicense

--- data_license_test.go
+++ data_license_test.go
@@ -1,7 +1,7 @@
 package thingfulx
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"testing"
 
@@ -10,7 +10,7 @@ import (
 
 // what should we test?
 // 1. the predefined ones have correct values
-// 2. we can add predefined ones to thing
+// 2. we can add predefined ones to thing // this is done inside thing_test.go
 // 3. we can add non-predefined ones to thing
 
 func TestCC0(t *testing.T) {
@@ -37,12 +37,52 @@ func TestCC0(t *testing.T) {
 	assert.NotContains(t, CC0V1.Prohibits, HighIncomeNationUse)
 }
 
-func TestLicense(t *testing.T) {
-	j, _ := json.Marshal(CCByNcV3)
-	fmt.Println(string(j))
+func TestCCCByV3(t *testing.T) {
 
-	// spew.Dump(CC_0_V1)
-	// spew.Dump(CC_BY_NC_V3)
-	// spew.Dump(CC_BY_SA_V4)
-	// spew.Dump(OGL_V3)
+	assert.Equal(t, CCByV3.Name, "Attribution 3.0 Unported (CC BY 3.0)")
+	assert.Equal(t, CCByV3.URL, "https://creativecommons.org/licenses/by/3.0/")
+	assert.Equal(t, CCByV3.LegalCodeURL, "https://creativecommons.org/licenses/by/3.0/legalcode")
+	// check Permits
+	assert.Contains(t, CCByV3.Permits, Reproduction)
+	assert.Contains(t, CCByV3.Permits, Distribution)
+	assert.Contains(t, CCByV3.Permits, DerivativeWorks)
+	assert.Contains(t, CCByV3.Permits, Sharing)
+
+	// check Requires
+	assert.Contains(t, CCByV3.Requires, Attribution)
+	assert.NotContains(t, CCByV3.Requires, Notice)
+	assert.NotContains(t, CCByV3.Requires, ShareAlike)
+	assert.NotContains(t, CCByV3.Requires, SourceCode)
+	assert.NotContains(t, CCByV3.Requires, Copyleft)
+	assert.NotContains(t, CCByV3.Requires, LesserCopyleft)
+
+	// check Prohibits
+	assert.NotContains(t, CCByV3.Prohibits, CommercialUse)
+	assert.NotContains(t, CCByV3.Prohibits, HighIncomeNationUse)
+}
+
+func TestCustomLicense(t *testing.T) {
+	thing := Thing{
+		Title:       "Title",
+		Description: "Description",
+		DataLicense: DataLicense{
+			Name: "custom made data license",
+			URL:  "http://some/url/to/the/license",
+			Permits: []string{
+				Reproduction,
+				Sharing,
+			},
+			Requires: []string{
+				Attribution,
+			},
+		},
+	}
+
+	assert.Equal(t, thing.DataLicense.Name, "custom made data license")
+	assert.Equal(t, thing.DataLicense.URL, "http://some/url/to/the/license")
+	assert.Contains(t, thing.DataLicense.Permits, Reproduction)
+	assert.Contains(t, thing.DataLicense.Permits, Sharing)
+	assert.Contains(t, thing.DataLicense.Requires, Attribution)
+	assert.NotContains(t, thing.DataLicense.Prohibits, CommercialUse)
+
 }
