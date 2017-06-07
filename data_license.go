6fe6c2f1db2d782c5eabad74272c1741c5985ae7
dotmancando <dotmancando@gmail.com>
2017-06-07 08:56:52 +0100

    update comment again

--- data_license.go
+++ data_license.go
@@ -5,42 +5,42 @@ package thingfulx
 const (
 
 	// Permissions
-	// Reproduction refers to "making multiple copies"
+	// Reproduction is "making multiple copies"
 	Reproduction = "https://creativecommons.org/ns#Reproduction"
 
-	// Distribution refers to "distribution, public display, and publicly performance"
+	// Distribution is "distribution, public display, and publicly performance"
 	Distribution = "https://creativecommons.org/ns#Distribution"
 
-	// DerivativeWorks refers to "distribution of derivative works"
+	// DerivativeWorks is "distribution of derivative works"
 	DerivativeWorks = "https://creativecommons.org/ns#DerivativeWorks"
 
-	// Sharing refers to "permits commercial derivatives, but only non-commercial distribution"
+	// Sharing is "permits commercial derivatives, but only non-commercial distribution"
 	Sharing = "https://creativecommons.org/ns#Sharing"
 
 	// Requirements
-	// Notice refers to "copyright and license notices be kept intact"
+	// Notice is "copyright and license notices be kept intact"
 	Notice = "https://creativecommons.org/ns#Notice"
 
-	// Attribution refers to "credit be given to copyright holder and/or author"
+	// Attribution is "credit be given to copyright holder and/or author"
 	Attribution = "https://creativecommons.org/ns#Attribution"
 
-	// ShareAlike refers to "derivative works be licensed under the same terms or compatible terms as the original work"
+	// ShareAlike is "derivative works be licensed under the same terms or compatible terms as the original work"
 	ShareAlike = "https://creativecommons.org/ns#ShareAlike"
 
-	// SourceCode refers to "source code (the preferred form for making modifications) must be provided when exercising some rights granted by the license"
+	// SourceCode is "source code (the preferred form for making modifications) must be provided when exercising some rights granted by the license"
 	SourceCode = "https://creativecommons.org/ns#SourceCode"
 
-	// Copyleft refers to "derivative and combined works must be licensed under specified terms, similar to those on the original work"
+	// Copyleft is "derivative and combined works must be licensed under specified terms, similar to those on the original work"
 	Copyleft = "https://creativecommons.org/ns#Copyleft"
 
-	// LesserCopyleft refers to "derivative works must be licensed under specified terms, with at least the same conditions as the original work; combinations with the work may be licensed under different terms"
+	// LesserCopyleft is "derivative works must be licensed under specified terms, with at least the same conditions as the original work; combinations with the work may be licensed under different terms"
 	LesserCopyleft = "https://creativecommons.org/ns#LesserCopyleft"
 
 	// Prohibitions
-	// CommercialUse refers to "exercising rights for commercial purposes"
+	// CommercialUse is "exercising rights for commercial purposes"
 	CommercialUse = "http://web.resource.org/cc/CommercialUse"
 
-	// HighIncomeNationUse refers to "use in a non-developing country"
+	// HighIncomeNationUse is "use in a non-developing country"
 	HighIncomeNationUse = "https://creativecommons.org/ns#HighIncomeNationUse"
 )
 
