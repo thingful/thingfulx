1136f9e0beb629e82368a277e9b42baa9f15c108
Sam Mulube <sam@umbrellium.co.uk>
2019-04-30 10:34:02 +0100

    Clean up client interface and tweak tests.
    * Remove the deprecated methods (only we were using)
    * Add post method
    * Add explicit context passed via the standard first parameter (for
      cancellation)

--- client.go
+++ client.go
@@ -1,17 +1,22 @@
 package thingfulx
 
 import (
+	"context"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-// ContextKey is a type alias used for context keys to avoid any issues wth clashing keys
+// ContextKey is a type alias used for context keys to avoid any issues wth
+// clashing keys
 type ContextKey string
 
 const (
-	// ClientToken is the key we use when setting a token on a context when
-	// making a request
+	// ClientToken is the key we use when setting a token on a context when making
+	// a request. Note it is the responsibility of the implementing indexer to
+	// determine how to send this token value to the data infrastructure (i.e.
+	// Authorization header, API Key in the query string or a header).
 	ClientToken = ContextKey("client-token")
 )
 
@@ -22,23 +27,27 @@ const (
 type Client interface {
 	// DoHTTP is a thin wrapper around the `Do` method on `net/http.Client`. It
 	// simply ensures we set the correct user agent header before making the
-	// request
-	DoHTTP(req *http.Request) (*http.Response, error)
+	// request. Note that unlike the wrapped standard library method we pass
+	// context explicitly as the first parameter.
+	DoHTTP(ctx context.Context, req *http.Request) (*http.Response, error)
 
 	// Get is a helper function on the client interface for the most common usage
-	// of the Client, making a simple Get request for a given URL, and returning
-	// a slice of bytes containing the HTTP response. This uses client.DoHTTP
-	// internally so that it ensures the correct user agent string is sent.
-	Get(urlStr string) ([]byte, error)
+	// of the Client, making a simple Get request for a given URL, and returning a
+	// slice of bytes containing the HTTP response. This uses client.DoHTTP
+	// internally so that it ensures the correct user agent string is sent. Note
+	// that unlike the wrapped standard library method we pass context explicitly
+	// as the first parameter.
+	GetHTTP(ctx context.Context, urlStr string) ([]byte, error)
 
-	// DoHTTPGetRequest is a helper function on the client interface for the most
-	// common usage of the Client, making a simple Get request for a given URL,
-	// and returning a slice of bytes containing the HTTP response. This uses
-	// client.DoHTTP internally so that it ensures the correct user agent string
-	// is sent.
-	//
-	// Deprecated: Use Client.Get instead
-	DoHTTPGetRequest(urlString string) ([]byte, error)
+	// PostHTTP defines a function for sending a POST request to a data
+	// infrastructure in order to obtain data. Typically we send GET requests, but
+	// some infrastructures may require POST requests. This function returns a
+	// slice of bytes for any HTTP OK response, and an error for anything else. If
+	// you require finer control of this you should use the DoHTTP method directly
+	// which returns the http.Response for processing as required. Note that unlike
+	// the wrapped standard library method we pass context explicitly as the first
+	// parameter.
+	PostHTTP(ctx context.Context, urlStr, contentType string, body io.Reader) ([]byte, error)
 }
 
 // NewClient is a constructor function that instantiates and returns a new
@@ -61,25 +70,32 @@ type client struct {
 
 // DoHTTP is a thin wrapper around the `Do` method on `net/http.Client`. It
 // simply ensures we set the correct user agent header before making the
-// request
-func (c *client) DoHTTP(req *http.Request) (*http.Response, error) {
+// request. We return an http.Response struct or an error.
+func (c *client) DoHTTP(ctx context.Context, req *http.Request) (*http.Response, error) {
 	// set the user agent here
 	req.Header.Add("User-Agent", c.userAgent)
+
+	// add our context to the request - this allows us to pass down any
+	// cancellation to where it needs to go.
+	req = req.WithContext(ctx)
 
 	return c.http.Do(req)
 }
 
-// Get is a helper function on the client interface for the most common usage
-// of the Client, making a simple Get request for a given URL, and returning a
-// slice of bytes containing the HTTP response. This uses client.DoHTTP
-// internally to ensure that the correct user agent string is sent.
-func (c *client) Get(urlStr string) (b []byte, err error) {
+// GetHTTP is a helper function on the client interface for the most common
+// usage of the Client, making a simple Get request for a given URL, and
+// returning a slice of bytes containing the HTTP response. This uses
+// client.DoHTTP internally to ensure that the correct user agent string is
+// sent. Note unlike the default http client interface we expose context as a
+// parameter in recommended style for context usage. We return a slice of bytes
+// or an error.
+func (c *client) GetHTTP(ctx context.Context, urlStr string) (b []byte, err error) {
 	req, err := http.NewRequest("GET", urlStr, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := c.DoHTTP(req)
+	resp, err := c.DoHTTP(ctx, req)
 	if err != nil {
 		return nil, err
 	}
@@ -105,13 +121,42 @@ func (c *client) Get(urlStr string) (b []byte, err error) {
 	return b, err
 }
 
-// DoHTTPGetRequest is a helper function on the client interface for the most
-// common usage of the Client, making a simple Get request for a given URL, and
-// returning a slice of bytes containing the HTTP response. This uses
-// client.DoHTTP internally to ensure that the correct user agent string is
-// sent.
-//
-// Deprecated: Use Client.Get instead
-func (c *client) DoHTTPGetRequest(urlStr string) ([]byte, error) {
-	return c.Get(urlStr)
+// PostHTTP is our implementation of the interface method for fetching data via
+// HTTP POST requests. It takes as parameters a context, the url to which data
+// should be posted and its content type, as well as the actual request body
+// wrapped in an io.Reader. Note that unlike the wrapped standard library method
+// we pass context explicitly as the first parameter. We return a slice of bytes
+// or an error.
+func (c *client) PostHTTP(ctx context.Context, urlStr, contentType string, body io.Reader) (b []byte, err error) {
+	req, err := http.NewRequest(http.MethodPost, urlStr, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", contentType)
+
+	resp, err := c.DoHTTP(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if cerr := resp.Body.Close(); err == nil && cerr != nil {
+			err = cerr
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusNotFound {
+			return nil, ErrNotFound
+		}
+		return nil, NewErrUnexpectedResponse(resp.Status)
+	}
+
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, err
 }
--- client_test.go
+++ client_test.go
@@ -1,89 +1,86 @@
 package thingfulx
 
 import (
-	"io/ioutil"
+	"context"
 	"net/http"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/thingful/httpmock"
+	"github.com/thingful/simular"
 )
 
-func TestDoHTTP(t *testing.T) {
-	duration := time.Duration(1) * time.Second
-
-	client := NewClient("thingful", duration)
-
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterStubRequest(
-		httpmock.NewStubRequest(
-			"GET",
-			"http://example.com/foo",
-			httpmock.NewStringResponder(200, "ok"),
-		),
-	)
-
-	req, err := http.NewRequest("GET", "http://example.com/foo", nil)
-	if err != nil {
-		t.Fatalf("Unexpected error, got %#v", err)
-	}
-
-	resp, err := client.DoHTTP(req)
-	if err != nil {
-		t.Fatalf("Unexpected error, got %#v", err)
-	}
-
-	defer func() {
-		if cerr := resp.Body.Close(); err == nil && cerr != nil {
-			err = cerr
-		}
-	}()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("Unexpected error, got %#v", err)
-	}
-
-	assert.Equal(t, body, []byte("ok"))
-}
+//func TestDoHTTP(t *testing.T) {
+//	duration := time.Duration(1) * time.Second
+//
+//	client := NewClient("thingful", duration)
+//
+//	httpmock.Activate()
+//	defer httpmock.DeactivateAndReset()
+//
+//	httpmock.RegisterStubRequest(
+//		httpmock.NewStubRequest(
+//			"GET",
+//			"http://example.com/foo",
+//			httpmock.NewStringResponder(200, "ok"),
+//		),
+//	)
+//
+//	req, err := http.NewRequest("GET", "http://example.com/foo", nil)
+//	if err != nil {
+//		t.Fatalf("Unexpected error, got %#v", err)
+//	}
+//
+//	resp, err := client.DoHTTP(req)
+//	if err != nil {
+//		t.Fatalf("Unexpected error, got %#v", err)
+//	}
+//
+//	defer func() {
+//		if cerr := resp.Body.Close(); err == nil && cerr != nil {
+//			err = cerr
+//		}
+//	}()
+//
+//	body, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		t.Fatalf("Unexpected error, got %#v", err)
+//	}
+//
+//	assert.Equal(t, body, []byte("ok"))
+//}
 
 func TestGet(t *testing.T) {
-	duration := time.Duration(1) * time.Second
+	timeout := time.Duration(1) * time.Second
 
-	client := NewClient("thingful", duration)
+	client := NewClient("thingful", timeout)
 
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+	simular.Activate()
+	defer simular.DeactivateAndReset()
 
-	httpmock.RegisterStubRequest(
-		httpmock.NewStubRequest(
-			"GET",
+	simular.RegisterStubRequests(
+		simular.NewStubRequest(
+			http.MethodGet,
 			"http://example.com",
-			httpmock.NewStringResponder(200, "ok"),
+			simular.NewStringResponder(200, "ok"),
 		),
 	)
 
-	t.Run("with new Get method", func(t *testing.T) {
-		got, err := client.Get("http://example.com")
+	ctx := context.Background()
+
+	t.Run("with Get method", func(t *testing.T) {
+		got, err := client.GetHTTP(ctx, "http://example.com")
 		assert.Nil(t, err)
 
 		assert.Equal(t, []byte("ok"), got)
 	})
 
-	t.Run("with old DoHTTPGetRequest method", func(t *testing.T) {
-		got, err := client.DoHTTPGetRequest("http://example.com")
-		assert.Nil(t, err)
-
-		assert.Equal(t, []byte("ok"), got)
-	})
+	assert.Nil(t, simular.AllStubsCalled())
 }
 
 func TestGetError(t *testing.T) {
-
 	testcases := []struct {
 		httpStatus int
 		expected   error
@@ -98,25 +95,60 @@ func TestGetError(t *testing.T) {
 		},
 	}
 
+	ctx := context.Background()
+
 	for _, testcase := range testcases {
 		duration := time.Duration(1) * time.Second
 
 		client := NewClient("thingful", duration)
 
-		httpmock.Reset()
+		simular.Reset()
 
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
+		simular.Activate()
+		defer simular.DeactivateAndReset()
 
-		httpmock.RegisterStubRequest(
-			httpmock.NewStubRequest(
-				"GET",
+		simular.RegisterStubRequests(
+			simular.NewStubRequest(
+				http.MethodGet,
 				"http://example.com",
-				httpmock.NewBytesResponder(testcase.httpStatus, nil),
+				simular.NewBytesResponder(testcase.httpStatus, nil),
 			),
 		)
 
-		_, err := client.Get("http://example.com")
+		_, err := client.GetHTTP(ctx, "http://example.com")
 		assert.Equal(t, testcase.expected, err)
 	}
+}
+
+func TestPost(t *testing.T) {
+	timeout := time.Duration(1) * time.Second
+
+	client := NewClient("thingful", timeout)
+
+	simular.Activate()
+	defer simular.DeactivateAndReset()
+
+	simular.RegisterStubRequests(
+		simular.NewStubRequest(
+			http.MethodPost,
+			"http://example.com",
+			simular.NewStringResponder(200, "ok"),
+			simular.WithBody(
+				strings.NewReader(`{"msg":"query"}`),
+			),
+			simular.WithHeader(
+				&http.Header{
+					"Content-Type": []string{"application/json"},
+					"User-Agent":   []string{"thingful"},
+				},
+			),
+		),
+	)
+
+	ctx := context.Background()
+
+	got, err := client.PostHTTP(ctx, "http://example.com", "application/json", strings.NewReader(`{"msg":"query"}`))
+	assert.Nil(t, err)
+
+	assert.Equal(t, []byte("ok"), got)
 }
